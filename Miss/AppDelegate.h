3bb5715c7a6985283efd922a68389435391f3aa7
djz000 <1345726337@qq.com>
2017-08-31 19:11:12 +0800

    miss

---
+++ Miss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Miss
+//
+//  Created by DJZ on 2017/8/31.
+//  Copyright © 2017年 djz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Miss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Miss
+//
+//  Created by DJZ on 2017/8/31.
+//  Copyright © 2017年 djz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
